391c594f4a3823f6ff7901d0da8bf00294b64995
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-10-08 15:06:19 +0900

    iDがなかった場合のバリデーションの追加

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,4 +1,5 @@
 const Workout = require('../models/workoutModel')
+const mongoose = require('mongoose')
 
 // get all workouts
 const getWorkouts = async (req, res) => {
@@ -8,7 +9,11 @@ const getWorkouts = async (req, res) => {
 
 // get a single workout
 const getWorkout = async (req, res) => {
-    const {id} = req.params
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No such workout'})
+    }
 
     const workout = await Workout.findById(id)
 
