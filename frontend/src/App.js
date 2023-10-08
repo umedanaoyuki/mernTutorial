2f1701e67caebff6dfdb19fb87bc5911a7ea4f5e
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-10-08 23:30:44 +0900

    画面の設定

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="App">
         <BrowserRouter>
+            <Navbar />
       <div className="pages">
           <Routes>
               <Route
